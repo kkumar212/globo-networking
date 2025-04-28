2bc295ac9496e0b200842e9c123f50d44b4122d6
Kapil <kapil.kumar2@ltimindtree.com>
2025-04-28 16:01:04 +0530

    chk

--- imports.tf
+++ imports.tf
@@ -1,48 +1,48 @@
-##################################################################################
-# IMPORTS
-##################################################################################
+# ##################################################################################
+# # IMPORTS
+# ##################################################################################
 
-import {
-  to = module.main.aws_vpc.this[0]
-  id = "vpc-0d8fc79e06ea1ec67" #VPC
-}
+# import {
+#   to = module.main.aws_vpc.this[0]
+#   id = "vpc-0d8fc79e06ea1ec67" #VPC
+# }
 
-import {
-  to = module.main.aws_subnet.public[0]
-  id = "subnet-0590c47355009b9af" #PublicSubnet1
-}
+# import {
+#   to = module.main.aws_subnet.public[0]
+#   id = "subnet-0590c47355009b9af" #PublicSubnet1
+# }
 
-import {
-  to = module.main.aws_subnet.public[1]
-  id = "subnet-07bc9b35ea688edc6" #PublicSubnet2
-}
+# import {
+#   to = module.main.aws_subnet.public[1]
+#   id = "subnet-07bc9b35ea688edc6" #PublicSubnet2
+# }
 
-import {
-  to = module.main.aws_internet_gateway.this[0]
-  id = "igw-077c81d07fb3d0ffd" #InternetGateway
-}
+# import {
+#   to = module.main.aws_internet_gateway.this[0]
+#   id = "igw-077c81d07fb3d0ffd" #InternetGateway
+# }
 
-import {
-  to = module.main.aws_route.public_internet_gateway[0]
-  id = "rtb-0a2f51e61184162cc_0.0.0.0/0" #DefaultPublicRoute
-}
+# import {
+#   to = module.main.aws_route.public_internet_gateway[0]
+#   id = "rtb-0a2f51e61184162cc_0.0.0.0/0" #DefaultPublicRoute
+# }
 
-import {
-  to = module.main.aws_route_table.public[0]
-  id = "rtb-0a2f51e61184162cc" #PublicRouteTable
-}
+# import {
+#   to = module.main.aws_route_table.public[0]
+#   id = "rtb-0a2f51e61184162cc" #PublicRouteTable
+# }
 
-import {
-  to = module.main.aws_route_table_association.public[0]
-  id = "subnet-0590c47355009b9af/rtb-0a2f51e61184162cc" #PublicSubnet1/PublicRouteTable
-}
+# import {
+#   to = module.main.aws_route_table_association.public[0]
+#   id = "subnet-0590c47355009b9af/rtb-0a2f51e61184162cc" #PublicSubnet1/PublicRouteTable
+# }
 
-import {
-  to = module.main.aws_route_table_association.public[1]
-  id = "subnet-07bc9b35ea688edc6/rtb-0a2f51e61184162cc" #PublicSubnet2/PublicRouteTable
-}
+# import {
+#   to = module.main.aws_route_table_association.public[1]
+#   id = "subnet-07bc9b35ea688edc6/rtb-0a2f51e61184162cc" #PublicSubnet2/PublicRouteTable
+# }
 
-import {
-  to = aws_security_group.ingress
-  id = "sg-0c4610a508e033e79" #NoIngressSecurityGroup
-}
+# import {
+#   to = aws_security_group.ingress
+#   id = "sg-0c4610a508e033e79" #NoIngressSecurityGroup
+# }
